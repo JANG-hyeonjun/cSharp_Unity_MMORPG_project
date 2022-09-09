8444de022b6a7893985e3234d29755823242f8f4
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-09-09 20:29:13 +0900

    Updated: 1.TextRpg2 is implemented which is made by OOP Programming.

---
+++ chpater1_C#_TEXTRPG2/TextRPG2_플레이어생성/TextRPG2_플레이어생성/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    //본인 타입은 내부로 가지고 있는 방향이 좋다
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+    
+    class Player : Creature
+    { 
+      
+        protected PlayerType _type = PlayerType.None;
+        
+        protected Player(PlayerType type) : base(CreatureType.Player)
+        {
+            _type = type;
+        }
+
+        public PlayerType GetPlayerType() { return _type; }
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+    }
+    
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+}
---
+++ chpater1_C#_TEXTRPG2/TextRPG2_플레이어생성/TextRPG2_플레이어생성/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            while(true)
+            {
+                game.Process();
+            }
+
+            //    //이름 공간이 같다는 전제하에 같은 파일에 있는 것 처럼 사용 가능
+            //    Player player = new Knight();
+            //    Player player2 = new Archer();
+            //    Monster monster = new Orc();
+            //    //게임 stage 나 로비를 관리해주는 객체가 있으면 좋지않을까 
+            //    int damage = player.GetAttack();
+            //    monster.OnDamaged(damage);
+            //    //재 사용성이 용이
+            //    //player2.OnDamaged(damage);
+        }
+    }
+}
