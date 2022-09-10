313b25d8714d26a0740a2ce6bd377bbdbbbc2b2b
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-09-10 15:35:27 +0900

    Updated: 1.study Csharp Syntax

---
+++ Chpater_1_CSharp/chapter1_C#_UsefulProgramming/Generic/Generic/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        //우리만의 자료구조 근데이게 정수로 쓸지 실수로 쓸지 문자열로 쓸지 정해야한다
+        //class MyintList
+        //{
+        //    int[] arr = new int[10];
+        //    object objArr = new object[10]
+        //}
+
+        //class MyShortList
+        //{
+        //    short[] arr = new short[10];
+        //}
+
+        //class MyfloatList
+        //{
+        //    float[] arr = new float[10];
+        //}
+
+        //또한 C#에는 있지만 C++에는 없는 문법이 있다.
+        // class MyList<T> where T : struct
+        //이는 T가 어떠한 형식에는 상관없지만 값 형식이어야 한다. 
+        //where T : class
+        //참조형식이어야 한다.
+        // where T : new()
+        //반드시 어떠한 인자를 받지 않는 기본 생성자가 있어야한다
+        // where T : Monster
+
+        class MyList<T>  
+        {
+            T[] arr = new T[10];
+        
+            public T GetItem(int i)
+            {
+                return arr[i];
+            }
+        }
+
+        class Monster
+        {
+
+        }
+
+        static void Test<T>(T input)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            //이렇게하면 불편하다. 그래서 C#은 어떤 타입이든 소화할수 있는 object를 제공한다
+            object obj = 3;
+            object obj2 = "hello world";
+
+            int num = (int)obj;
+            string str = (string)obj2;
+
+            //그러면 obj랑 var이 똑같은거 아니냐 할수 있는데 
+            //var은 뒤에 있는 자료를 보고 컴파일러가 판단 그래서 알아서 변환 
+            //object는 타입이 진짜 object 그런 자료형이라는 것이다.
+
+            //그러면 모든 자료형을 선언할 필요 없이 obj를 쓰면 되는것 아니냐 할 수 있는데
+            //object는 힙에 저장이 되어지는 참조형 -> 따라서 힙에서 스택에 가지고 오고 캐스팅연산을
+            //해야 비로소 우리가 그냥 쓰는것과 동일한 단계까지 온것이다. 결론적으로 시간이 걸리게 된다.
+
+            MyList<int> myIntList = new MyList<int>();
+            int item = myIntList.GetItem(0);
+            
+            MyList<short> mySHortList = new MyList<short>();
+            MyList<Monster> myMonsterList = new MyList<Monster>();
+
+            //Test<float>(3.0f);
+        }
+    }
+}
---
+++ Chpater_1_CSharp/chapter1_C#_UsefulProgramming/Interface/Interface/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        abstract class Monster
+        {
+            public abstract void Shout();
+        }
+        interface IFlyable //나의 기능을 가지고 있는 얘라면 반드시 Fly라는 인터페이스 함수를 제공해야해
+        {
+            void Fly(); //이런 모양이다 이런 설계다
+        }
+
+        class Orc : Monster
+        {
+            public override void Shout()
+            {
+                Console.WriteLine("록타르 오가르!");   
+            }
+        }
+
+        class FlyableOrc : Orc, IFlyable
+        {
+            public void Fly()
+            {
+                Console.WriteLine("펄럭펄럭");
+            }
+        }
+        //C#은 다중상속이 안되는데 이는 죽음의 다이아몬드 구조문제
+        //예륻들어 Orc 와 Skelton class를 상속받는 어떠한 경우가 있었다 하면
+        //base의 shout()을 호출하면 어떠한 클래스의 멤버함수가 호출되는지 알수가 없다.
+        //그래서 막아놓음 그래서 인터페이스는 물려주지만 구현부는 너가 알아서 해 하는 문법이 있으면 된다.
+
+        //그래서 CSharp은 interface 키워드를 제공 인터페이스는 여러개를 받아도 상관 없다.
+
+        class Skleton : Monster
+        {
+            public override void Shout()
+            {
+                Console.WriteLine("꾸에에엑!");
+            }
+        }
+        //근데 지금 이상태는 shout을 선언을 해줘도 되고 안해도 되는것이다.
+        //그래서 스켈레톤을 구현을 안하면 Monster class의 shout이 구현이 될것이다.
+
+        static void DoFly(IFlyable flyable)
+        {
+            //왜 받는 매개변수를 IFlyable로 했는지를 생각해보면 범용적으로 static을 사용해야 하니까
+            //또 하나 발견한 사실은 C++은 업캐스팅 되어 있는것을 다운 캐스팅을 해서 그 함수를 사용할 수 있지만
+            //인터페이스는 왼쪽의 자료형을 따르는것이 아닌거 같다. (애초에 인터페이스 생성자를 통해 인스턴스를 할당 할수도 없다)
+            flyable.Fly();
+        }
+
+        static void Main(string[] args)
+        {
+            //추상 클래스와 인터페이스
+            //Monster monster = new Monster();
+            FlyableOrc orc = new FlyableOrc();
+            Orc orc2 = new FlyableOrc();
+            //orc2.Fly();
+            DoFly(orc);
+        }
+    }
+}
