e2f12dbd4a16d294889f749dcf29bec789ed93d2
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-10-02 23:04:42 +0900

    Updated: 1. CSharp data structure related with dynmaic array and linked list is commited

---
+++ Chapter_2_DataStructure_Algorithm/Chapter2_C#_LinearDataStructure/DynamicArray/cSharp/cSharp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize();
+            
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0; //마지막으로 측정한 시간
+            while (true)
+            {
+
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                //int elapsedTick = currentTick - lastTick;
+                //만약에 걍과한 시간이 1/30초보다 작다면 continue
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                #endregion
+                //구현을 했는데 보기 싫다면 이렇게 하면 된다.
+
+
+                lastTick = currentTick;
+
+                //현재시간을 이렇게 가지고 있는다. 엄밀히 말하면 정확한 시간의 느낌이 아니라
+                //시스템이 시작한 이후에 경과된 밀리 세컨드를 준다.
+
+                //프레임관리 FPS 60 프레임 Ok 30프레임 이하로 끊긴다.
+                //프레임 간단하게 얘기해서 현재 이 루프가 1초에 얼마나 실행 되는가 
+
+                //입력
+
+                //로직
+
+                //렌더링
+
+                //즉 부드럽게 보인다는 것은 아주 짧은 시간내 연산을 마치고 화면에 바로 보여주는 것
+                //그래서 프레임 관리를 하기 위해서는 시간을 계속 측정하고 있어야 한다.
+
+
+                //우리는 이제 이 이맵에 텍스처 벽인지 물인지 불인지 관리를 해야하며 
+                //관리하는 클래스가 당연히 필요 하다. 즉 아래 있는것을 클래스화 할필요가 있다.
+                Console.SetCursorPosition(0, 0);
+                
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Chapter_2_DataStructure_Algorithm/Chapter2_C#_LinearDataStructure/LinkedList/CSharp/CSharp/Board.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class MyList<T>
+    {
+        const int DEFAULT_SIZE = 1;
+        T[] _data = new T[DEFAULT_SIZE];
+
+        public int Count = 0; //실제로 사용중인 데이터 개수
+        public int Capacity { get { return _data.Length; } } // 예약된 데이터 개수
+
+        // O(1) // if(Count >= Capacity)는 무시한다. (이사비용은 무시한다.)
+        public void Add(T item)
+        {
+            //1.공간이 충분히 남아 있는지 확인한다.
+            if (Count >= Capacity)
+            {
+                //공간을 다시 늘려서 확보해야 한다.
+                T[] newArray = new T[Count * 2];
+                //2라는 숫자는 growth factor;
+                for (int i = 0; i < Count; i++)
+                    newArray[i] = _data[i];
+                _data = newArray;
+            }
+
+            //2. 공간에다가 데이터를 넣어준다. (여기 왔다는건 공간은 충분)
+            _data[Count] = item;
+            Count++;
+        }
+        //C#에서 이런걸 인덱서 라고 한다.
+        //O(1)
+        public T this[int index]
+        {
+            //for situation of int temp = _data2[2]; //index 안에는 2가
+            get { return _data[index]; }
+            //for situation of _data2[2] = 3; //value안에 3이 들어옴
+            set { _data[index] = value; }
+        }
+
+        //O(N)
+        public void RemoveAt(int index)
+        {
+            // 101 102 103 104 105
+            for (int i = index; i < Count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _data[Count - 1] = default(T);
+            //어떤 자료형이 오든 그 형의 기본 초기값으로 해달라 클래스면 널 인트면 0
+
+            Count--;
+        }
+
+    }
+
+    class Board
+    {
+        // 사용할때 어떤식으로든 초기화를 해서 맵정보를 아래함수에서 불러와서 맵을 구성한다.
+        // 정보를 1차원 배열이나 2차원 배열로 들고 있을 수도 있다.
+        public int[] _data = new int[25];
+        public MyList<int> _data2 = new MyList<int>();
+        public LinkedList<int> _data3 = new LinkedList<int>();
+        //결국 프로그래밍이란 데이터를 어떻게 저장하고 어떻게 불러와서 이를 어떻게 잘 활용할지가 
+        // 정말 중요한 분야 
+
+        //연결리스트 구현 연습
+    
+    
+    }
+}
