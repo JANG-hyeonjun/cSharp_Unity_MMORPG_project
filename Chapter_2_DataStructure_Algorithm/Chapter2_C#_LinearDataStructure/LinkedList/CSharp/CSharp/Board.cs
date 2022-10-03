03b1b04308f8bada93ac4771992554d0aa4d31c8
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-10-03 19:33:16 +0900

    update: 1. A linked list data  is implemented by CSharp.

--- Chapter_2_DataStructure_Algorithm/Chapter2_C#_LinearDataStructure/LinkedList/CSharp/CSharp/Board.cs
+++ Chapter_2_DataStructure_Algorithm/Chapter2_C#_LinearDataStructure/LinkedList/CSharp/CSharp/Board.cs
@@ -6,70 +6,93 @@ using System.Threading.Tasks;
 
 namespace CSharp
 {
-    class MyList<T>
+    class MyLinkedListNode<T>
     {
-        const int DEFAULT_SIZE = 1;
-        T[] _data = new T[DEFAULT_SIZE];
+        public T Data;
+        public MyLinkedListNode<T> Next;
+        public MyLinkedListNode<T> Prev;
+    }
 
-        public int Count = 0; //실제로 사용중인 데이터 개수
-        public int Capacity { get { return _data.Length; } } // 예약된 데이터 개수
-
-        // O(1) // if(Count >= Capacity)는 무시한다. (이사비용은 무시한다.)
-        public void Add(T item)
-        {
-            //1.공간이 충분히 남아 있는지 확인한다.
-            if (Count >= Capacity)
-            {
-                //공간을 다시 늘려서 확보해야 한다.
-                T[] newArray = new T[Count * 2];
-                //2라는 숫자는 growth factor;
-                for (int i = 0; i < Count; i++)
-                    newArray[i] = _data[i];
-                _data = newArray;
-            }
-
-            //2. 공간에다가 데이터를 넣어준다. (여기 왔다는건 공간은 충분)
-            _data[Count] = item;
-            Count++;
-        }
-        //C#에서 이런걸 인덱서 라고 한다.
+    class MyLinkedList<T>
+    {
+        public int Count = 0;
+        //총 노드가 몇개
+        public MyLinkedListNode<T> Head = null; //첫번쨰
+        public MyLinkedListNode<T> Tail = null; //마지막    
+        
         //O(1)
-        public T this[int index]
+        public MyLinkedListNode<T> AddLast(T data)
         {
-            //for situation of int temp = _data2[2]; //index 안에는 2가
-            get { return _data[index]; }
-            //for situation of _data2[2] = 3; //value안에 3이 들어옴
-            set { _data[index] = value; }
-        }
+            MyLinkedListNode<T> newRoom = new MyLinkedListNode<T>();
+            newRoom.Data = data;
+            //만약 방이 아예 없었다면, 새로 추가한 첫번째 방이 곧 Head이다.
+            if (Head == null)
+                Head = newRoom;
 
-        //O(N)
-        public void RemoveAt(int index)
-        {
-            // 101 102 103 104 105
-            for (int i = index; i < Count - 1; i++)
+            // 101 102 103 104
+            //기존에 [마지막 방] 과 [새로 추가되는 방]을 연결한다.
+            if (Tail != null)
             {
-                _data[i] = _data[i + 1];
+                Tail.Next = newRoom;
+                newRoom.Prev = Tail;
             }
-            _data[Count - 1] = default(T);
-            //어떤 자료형이 오든 그 형의 기본 초기값으로 해달라 클래스면 널 인트면 0
+
+            //[새로 추가되는 방]을 [마지막 방]으로 인정한다.
+            Tail = newRoom;
+            Count++;
+            return newRoom;
+        }
+        
+        //삭제를 요청한 방이 무조건 RoomList에 들어간다 가정
+        // 101 102 103 104 105
+        
+        //O(1)
+        public void Remove(MyLinkedListNode<T> room)
+        {
+            //[기존의 첫번째 방의 다음방] 을 [첫번째 방으로] 인정한다.
+            if (Head == room)
+                Head = Head.Next;
+
+            // [기존의 마지막 방의 이전방 ] 을 [마지막 방으로] 인정한다.
+            if (Tail == room)
+                Tail = Tail.Prev;
+
+            if (room.Prev != null)
+                room.Prev.Next = room.Next;
+
+            if (room.Next != null)
+                room.Next.Prev = room.Prev;
 
             Count--;
         }
-
+    
     }
+
+
 
     class Board
     {
         // 사용할때 어떤식으로든 초기화를 해서 맵정보를 아래함수에서 불러와서 맵을 구성한다.
         // 정보를 1차원 배열이나 2차원 배열로 들고 있을 수도 있다.
         public int[] _data = new int[25];
-        public MyList<int> _data2 = new MyList<int>();
-        public LinkedList<int> _data3 = new LinkedList<int>();
+        public MyLinkedList<int> _data3 = new MyLinkedList<int>();
         //결국 프로그래밍이란 데이터를 어떻게 저장하고 어떻게 불러와서 이를 어떻게 잘 활용할지가 
         // 정말 중요한 분야 
 
         //연결리스트 구현 연습
-    
-    
+        public void Initialize()
+        {
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            MyLinkedListNode<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
+
+            //핵심은 링크드 리스트는 인덱스 access가 지원을 하지 않는다.
+            //당연히 시간이 오래걸리기 때문 그래서 라이브러리 지원이 없다.
+
+            _data3.Remove(node);
+
+        }
     }
 }
