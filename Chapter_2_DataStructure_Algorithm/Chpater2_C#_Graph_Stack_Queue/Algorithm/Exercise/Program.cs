a37377727a2eb74d73f4695d005434e8bfc9e30f
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-10-23 18:34:11 +0900

    study find path & Graph

--- Chapter_2_DataStructure_Algorithm/Chpater2_C#_Graph_Stack_Queue/Algorithm/Exercise/Program.cs
+++ Chapter_2_DataStructure_Algorithm/Chpater2_C#_Graph_Stack_Queue/Algorithm/Exercise/Program.cs
@@ -11,8 +11,8 @@ namespace Exercise
             {0, 1, 0, 1, 0, 0 },
             {1, 0, 1, 1, 0, 0 },
             {0, 1, 0, 0, 0, 0 },
-            {1, 1, 0, 0, 0, 0 },
-            {0, 0, 0, 0, 0, 1 },
+            {1, 1, 0, 0, 1, 0 },
+            {0, 0, 0, 1, 0, 1 },
             {0, 0, 0, 0, 1, 0 }
          };
 
@@ -22,15 +22,68 @@ namespace Exercise
             new List<int>() { 1, 3 },
             new List<int>() { 0, 2, 3 },
             new List<int>() { 1 },
-            new List<int>() { 0, 1},
-            new List<int>() { 5},
+            new List<int>() { 0, 1, 4},
+            new List<int>() { 3, 5},
             new List<int>() { 4 },
         };
 
-        private bool[] visited = new bool[6];
+        #region BFS 관련코드
+        public void BFS(int start)
+        {
+            bool[] found = new bool[6];
+
+            //내가 어떤 정점으로 부터 왔는지 보고 싶다
+            int[] parent = new int[6];
+            //그리고 오기까지 얼마만큼의 거리가 걸렸는지 알고 싶다.
+            int[] distance = new int[6];
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+            parent[start] = 0;
+            distance[start] = 0;
+
+            while(q.Count > 0)
+            {
+                int now = q.Dequeue();
+                Console.WriteLine(now);
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0)//인접하지 않았으면 스킵
+                        continue;
+                    if (found[next]) //이미 발견된 애라면 스킵
+                        continue;
+
+                    q.Enqueue(next);
+                    found[next] = true;
+                    parent[next] = now;
+                    distance[next] = distance[now] + 1;
+                }
+            }
+        }
+        /*
+         * BFS는 길찾기 관련에 정말 많이 사용되고 DFS는 더 다양한곳에 많이 사용된다
+         */
+        #endregion
+
+
+        #region DFS 관련코드
+        //배열버전 굳이 위처럼 안해도이렇게 가능
+        //int[][] adj3 = new int[][]
+        //{
+        //    new int[] { 1, 3 },
+        //    new int[] { 0, 2, 3 },
+        //    new int[] { 1 },
+        //    new int[] { 0, 1, 4},
+        //    new int[] { 3, 5},
+        //    new int[] { 4 },
+        //};
+
+        //bool[] visited = new bool[6];
         //1) 우선 now부터 방문 하고,
         //2) now와 연결된 정점들을 하나씩 확인해서, [아직 미발변(미방문)] 상태라면 방문한다.
-        public void DFS(int now)
+        public void DFS(int now, bool[] visited)
         {
             Console.WriteLine(now);
             visited[now] = true; //1) 우선 now부터 방문 하고,
@@ -44,12 +97,12 @@ namespace Exercise
                 if (visited[next] == true) //이미 방문했으면 스킵
                     continue;
 
-                DFS(next);
+                DFS(next,visited);
             }
         }
         
         //인접 리스트 버전
-        public void DFS2(int now)
+        public void DFS2(int now,bool[] visited)
         {
             Console.WriteLine(now);
             visited[now] = true; //1) 우선 now부터 방문 하고,
@@ -59,20 +112,21 @@ namespace Exercise
                 if (visited[next] == true) //이미 방문했으면 스킵
                     continue;
 
-                DFS2(next);
+                DFS2(next,visited);
             }
         }
         //근데 길이 끊어져 있다면? 모든정점을 돌면서 방문이 되지 않은 정점이 있다면 
         //그 정점을 기준으로 또 DFS를 실행해야 한다. 
         public void SearchAll()
         {
-            visited = new bool[6];
+            bool[] visited = new bool[6];
             for(int now = 0; now < 6; ++now)
             {
                 if (visited[now] == false)
-                    DFS(now);
+                    DFS(now,visited);
             }
         }
+        #endregion
     }
 
     class Program
@@ -143,9 +197,8 @@ namespace Exercise
             //BFS (Breadth First Search 너비 우선 탐색)
             Graph graph = new Graph();
             //graph.DFS2(3);
-
-            graph.SearchAll();
-
+            //graph.SearchAll();
+            graph.BFS(0);
             #endregion
 
         }
