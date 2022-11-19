e3d489063e0ee39c652bde9fbb4e0662c2747909
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-11-20 00:46:27 +0900

    Modified: 1. Manger is made by singleton Pattern.

---
+++ Chpater_3_Unity/MMO_Unity/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance; //유일성이 보장된다.
+    public static Managers Instance { get { init(); return s_instance; }} //유일한 매니저를 갖고 온다. 개선
+    //public static Managers GetInstance() { init(); return s_instance; } //유일한 매니저를 갖고 온다.
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    static void init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            //근데 이 object 더이상 삭제 추가 되면 안되니까
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+        // 초기화 
+        //instance = this; //누군가는 인스턴스를 만들어 주어야 하니 
+        //GameObject go = GameObject.Find("@Managers");
+        //Managers mg = go.GetComponent<Managers>();
+        //하이러라키 뷰에서 @Managers가 없다면 instance가 널이 된다. 그러면 여기서 이걸 사용하면 
+        //크래쉬가 일어난다. 
+    }
+
+}
+
