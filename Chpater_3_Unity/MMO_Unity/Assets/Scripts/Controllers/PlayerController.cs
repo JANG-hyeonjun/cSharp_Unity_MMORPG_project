4858034e32fddb8551798523deac1b2ce214e4d6
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2023-09-01 00:03:32 +0900

    Study: Camera Qatar View has been implemented..

---
+++ Chpater_3_Unity/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//게임에서는 아래 두가지로 사용되어짐
+//1.위치 벡터
+//2.방향 벡터
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+    void Start()
+    {
+        //일단 한번 끊고
+        Managers.Input.KeyAction -= OnKeyboard;
+        // 여기서 추가해 주세요 이벤트를 한번만 허용
+        Managers.Input.KeyAction += OnKeyboard;
+    }
+
+    //GameObject (Player)
+        //Transform
+        //PlayerController(*)
+
+   
+    void Update() //한프레임당 호출 됨 -> 60 프레임 이라고 하고 60분의 1초마다 호출되는 것 
+    {
+        //여기서 UI checking 이런거뿐만아니라 수많은 입력이 들어오는데 
+        //그러면 부하가 온다.
+        
+        //플레이어 100개를 만든다고 하면 이 업데이트문을 100개의 플레이어마다 키를 체크
+        // 그래서 공용으로 사용할 수 있는 매니저를 두고 그 이벤트를 건너 받는게 좋지 않을까 
+    }
+
+    void OnKeyboard()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //특정 방향을 바라보게 끔
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+    }
+}
