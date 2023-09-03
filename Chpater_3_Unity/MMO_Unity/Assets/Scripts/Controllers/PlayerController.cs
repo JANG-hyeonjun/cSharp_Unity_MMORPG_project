b455f0b72027ea1e60e8a539cd5e07e48885b35e
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2023-09-03 10:39:14 +0900

    Study: 캐릭터 애니메이션 구현(기초 & Blending)

--- Chpater_3_Unity/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ Chpater_3_Unity/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -27,10 +27,12 @@ public class PlayerController : MonoBehaviour
     }
 
     //GameObject (Player)
-        //Transform
-        //PlayerController(*)
+    //Transform
+    //PlayerController(*)
 
-   
+
+    float wait_run_ratio = 0;
+
     void Update() //한프레임당 호출 됨 -> 60 프레임 이라고 하고 60분의 1초마다 호출되는 것 
     {
         //여기서 UI checking 이런거뿐만아니라 수많은 입력이 들어오는데 
@@ -50,11 +52,31 @@ public class PlayerController : MonoBehaviour
             {
                 float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
                 transform.position += dir.normalized * moveDist;
-                transform.LookAt(_destPos);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+                
+                //transform.LookAt(_destPos);
             }
-            
-        
         }
+
+        if(_moveToDest)
+        {
+            wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
+
+            Animator anim = GetComponent<Animator>();
+            anim.SetFloat("wait_run_ratio", wait_run_ratio);
+            anim.Play("WAIT_RUN");
+        }
+        else
+        {
+
+            wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
+            Animator anim = GetComponent<Animator>();
+            anim.SetFloat("wait_run_ratio", wait_run_ratio);
+            anim.Play("WAIT_RUN");
+        }
+        
+        
 
     }
 
@@ -89,9 +111,6 @@ public class PlayerController : MonoBehaviour
 
     void OnMouseClicked(Define.MouseEvent evt)
     {
-        if (evt != Define.MouseEvent.Click)
-            return;
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
