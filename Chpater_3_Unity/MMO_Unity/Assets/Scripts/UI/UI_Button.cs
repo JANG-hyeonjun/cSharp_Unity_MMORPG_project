ac07280021d56c9775251c15796080098efa4c2a
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2023-09-26 00:28:11 +0900

    Study: UI_EventHandler and Utility function has been made..

--- Chpater_3_Unity/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
+++ Chpater_3_Unity/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 public class UI_Button : UI_Base
 {
@@ -21,22 +22,29 @@ public class UI_Button : UI_Base
         TestObject,
     }
 
+    enum Images
+    {
+        ItemIcon,
+    }
+
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
         Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
 
-        GetText((int)Texts.ScoreText).text = "Bind Test";
-    
+        GetButton((int)Buttons.PointButton).gameObject.AddUIEvent(OnButtonClicked);
+
+        GameObject go = GetImage((int)Images.ItemIcon).gameObject;
+        AddUIEvent(go, (PointerEventData data) => { go.transform.position = data.position; }, Define.UIEvent.Drag);
     }
-
-  
 
     int _score = 0;
 
-    public void OnButtonClicked()
+    public void OnButtonClicked(PointerEventData data)
     {
         _score++;
+        GetText((int)Texts.ScoreText).text = $"점수: {_score}";
     }
 }
---
+++ Chpater_3_Unity/MMO_Unity/Assets/Scripts/Utils/Util.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util
+{
+    //이걸 왜 만드냐면 GetComponentsInChildren 이나 GetChild를 쓰는데 찾는 T가 Monobehavior나 Componet의 서브클래스여야 하는데 GameObject는 서브 클래스가 아니다.
+    
+    public static T GetOrAddComponent<T>(GameObject go) where T : UnityEngine.Component
+    {
+        T component = go.GetComponent<T>();
+        if (component == null)
+            component = go.AddComponent<T>();
+
+        return component;
+    }
+    
+    public static GameObject FindChild(GameObject go, string name = null, bool recursive = false) 
+    {
+        Transform transform = FindChild<Transform>(go, name, recursive);
+        if (transform == null)
+            return null;
+        return transform.gameObject;
+    }
+    public static T FindChild<T>(GameObject go, string name = null, bool recursive = false) where T : UnityEngine.Object
+    {
+        if (go == null)
+            return null;
+
+        if(recursive == false)
+        {
+            for(int i = 0; i < go.transform.childCount; i++)
+            {
+                Transform transforms = go.transform.GetChild(i);
+                if (string.IsNullOrEmpty(name) || transforms.name == name)
+                {
+                    T component = transforms.GetComponent<T>();
+                    if (component != null)
+                        return component;
+                }
+            }
+        }
+        else
+        {
+            foreach(T component in go.GetComponentsInChildren<T>())
+            {
+                if (string.IsNullOrEmpty(name) || component.name == name)
+                    return component;
+            }
+        }
+
+        return null;
+    }
+}
