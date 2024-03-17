d4f1b3fb84cf31a5cd49ea8fc17dd79c7ae86b65
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2024-03-17 09:46:27 +0900

    Study: 메모리 베리어 및 캐시이론

---
+++ Chapter_4_Server/Server/ServerCore/캐시이론.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[10000, 10000];
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
+            }
+
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
+            }
+        
+            //시간차이가 벌어지는 이유 공간적 접근성 적인 부분에서 차이가 벌어진다.
+        }
+    }
+}
